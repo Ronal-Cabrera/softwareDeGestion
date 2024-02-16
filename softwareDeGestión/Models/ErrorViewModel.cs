fd051a1c84bfc13b55b208938a3988da1a3fc33b
Ronal-Cabrera <116983660+Ronal-Cabrera@users.noreply.github.com>
2024-02-16 11:22:01 -0600

    Agregar archivos de proyecto.

---
+++ softwareDeGestión/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace softwareDeGestión.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
